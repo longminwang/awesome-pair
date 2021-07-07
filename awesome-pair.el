3a22f87a5f00f769795780bf296f905fe6cd2149
Longmin Wang <longminwang@gmail.com>
2021-07-07 18:51:41 +0800

    Revert "do not indent \[ in org-mode"
    This reverts commit f8ea54f78adf8df56dd5f5e63715464e0a998408.

--- awesome-pair.el
+++ awesome-pair.el
@@ -346,15 +346,6 @@
         (derived-mode-p 'org-mode))
     (cond
      ((= (char-before) ?\\)
-      (if (derived-mode-p 'org-mode)
-          (progn
-            (backward-char 1)
-            (kill-region (save-excursion
-                           (search-backward-regexp "[^ \t\n]" nil t)
-                           (forward-char 2)
-                           (point))
-                         (point))
-            (forward-char 1)))
       (insert "[\\]")
       (backward-char 2)
       (newline 2)
